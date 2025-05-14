936a11026c5172205cd86b3997fa82e1387049bd
luigilunardon <l.lunardon@nchain.com>
2025-05-14 12:26:53 +0200

    refactor: improved config loading

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,6 @@
 
 from pathlib import Path
 
-import tomllib
-
 # Paths
 BASE_DIR = Path(__file__).resolve().parent.parent
 OUTPUT_DIR = BASE_DIR / "outputs"
@@ -12,12 +10,3 @@ RECOVERY_DIR = SRC_DIR / "json" / "recovery"
 
 PROMPT_FILE = SRC_DIR / "json" / "prompt.json"
 CONFIG_FILE = SRC_DIR / "config.toml"
-
-
-def load_config(filename):
-    """Load config from toml file."""
-    with Path.open(filename, "rb") as f:
-        return tomllib.load(f)
-
-
-config = load_config(CONFIG_FILE)
---
+++ src/utils/load_data.py
@@ -0,0 +1,64 @@
+"Load keys in the .env file."
+
+import json
+from pathlib import Path
+
+import tomllib
+from dotenv import get_key, set_key
+
+from constants import RECOVERY_DIR, SRC_DIR
+import shutil
+
+def load_config(filename):
+    """Load config from toml file.
+
+    Args:
+        filename (Path): Path of the toml file.
+
+    """
+    with Path.open(filename, "rb") as f:
+        return tomllib.load(f)
+
+
+def load_api_key(keys):
+    """Initialise the .env file with API keys.
+
+    Args:
+        keys (list): a list of API keys to initialize.
+
+    """
+    env_path = SRC_DIR / ".env"
+    env_path.touch()
+
+    for key in keys:
+        if get_key(env_path, f"{key.upper()}_API_KEY", encoding='utf-8') is None:
+            value = input(f"{key.title()} API key:")
+            set_key(dotenv_path=env_path, key_to_set=f"{key.upper()}_API_KEY", value_to_set=value)
+
+
+def load_tasks_state(query):
+    """Load tasks recovery file if available.
+
+    Args:
+        query (str): The user query.
+
+    """
+    for directory in RECOVERY_DIR.iterdir():
+        recovery_file_path = directory / "task.json"
+        if recovery_file_path.exists():
+            with Path.open(recovery_file_path) as file:
+                recovery_file = json.load(file)
+            if query == recovery_file["query"]:
+                answer = ""
+                while answer not in {"y", "n"}:
+                    answer = input(
+                        f"Do you want to reuse the recovery file {recovery_file_path}? (y/n): "
+                    )
+                    if answer:
+                        answer = answer[0].lower()
+                if answer == "y":
+                    recovery_file["load_recovery"] = True
+                    return recovery_file
+                else:
+                    shutil.rmtree(directory)
+    return {"query": query, "load_recovery": False}
